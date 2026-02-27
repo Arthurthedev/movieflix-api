4125aed66a4f25e553026c62fc430a19f62a8180
Arthur Moraes <arthur.m.thurler@gmail.com>
2026-02-27 20:41:08 -0300

    adicionando serviço de cadastro de filmes e tratando erros

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,43 @@ import express from "express";
 import { PrismaClient } from "../generated/prisma/client.js";
 const port = 3000;
 const app = express();
+app.use(express.json());
 const prisma = new PrismaClient();
+
 app.get("/movies", async (req, res) => {
     const movies = await prisma.movie.findMany({
-        orderBy:{
-            title: "asc"
+        orderBy: {
+            title: "asc",
         },
-        include:{
+        include: {
             genres: true,
-            languages: true
-        }
+            languages: true,
+        },
     });
-    res.json(movies)
-   
+    res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } =
+        req.body;
+
+try {
+     await prisma.movie.create({
+        data: {
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            release_date: new Date(release_date),
+        },
+    });
+
+} catch (error) {
+    return res.status(500).send({message: "Falha ao cadastrar um filme"})
+}
+  res.status(201).send();  
 });
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta http://localhost:${port}`);
-     console.log(process.env.DATABASE_URL);
 });
